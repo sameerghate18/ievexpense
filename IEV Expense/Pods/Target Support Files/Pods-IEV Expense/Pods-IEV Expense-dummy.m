87b2dc3de45ff48b4d38492e32efd28fec414c34
Sameer Ghate <sameer.ghate@vodafone.com>
2019-08-26 19:16:04 +0530

    removed all files

--- IEV Expense/Pods/Target Support Files/Pods-IEV Expense/Pods-IEV Expense-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IEV_Expense : NSObject
-@end
-@implementation PodsDummy_Pods_IEV_Expense
-@end
--- IEV Expense/Pods/Target Support Files/Pods-IEV ExpenseTests/Pods-IEV ExpenseTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IEV_ExpenseTests : NSObject
-@end
-@implementation PodsDummy_Pods_IEV_ExpenseTests
-@end
